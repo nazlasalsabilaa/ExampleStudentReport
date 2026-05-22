ac5129016d77ae854b329e6f8f823af74326c3c5
Guntur <gunturpdng@gmail.com>
2026-05-22 22:41:02 +0000

    feat(route) : add redirect from / to /login

--- src/main/kotlin/com/example/studentreport/web/controller/WebUIController.kt
+++ src/main/kotlin/com/example/studentreport/web/controller/WebUIController.kt
@@ -2,9 +2,15 @@ package com.example.studentreport.web.controller
 
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.servlet.view.RedirectView
 
 @Controller
 class WebUIController {
+    @GetMapping("/")
+    fun rootRedirect(): RedirectView {
+        return RedirectView("/login")
+    }
+
     @GetMapping("/login")
     fun login() = "login"
 
