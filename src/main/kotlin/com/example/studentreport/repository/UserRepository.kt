8a59ca11a5e55bb0f54ddc95c4ecad980121e5b6
guntur <gunturpdng@gmail.com>
2026-06-04 05:24:56 +0800

    feat(user): user repository now can handle dynamic queries

--- src/main/kotlin/com/example/studentreport/repository/UserRepository.kt
+++ src/main/kotlin/com/example/studentreport/repository/UserRepository.kt
@@ -2,11 +2,12 @@ package com.example.studentreport.repository
 
 import com.example.studentreport.entity.User
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor
 import org.springframework.stereotype.Repository
 import java.util.UUID
 
 @Repository
-interface UserRepository : JpaRepository<User, UUID> {
+interface UserRepository : JpaRepository<User, UUID>, JpaSpecificationExecutor<User> {
     fun findByEmail(email: String): User?
     fun existsByEmail(email: String): Boolean
 }
